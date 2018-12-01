1b09158d52b3f88756443f3c9c5b3e54d0538bfc
cesnikb <cesnik.b@gmail.com>
2018-12-01 14:39:25 +0100

    Added: - Sample chooser - Synth player

--- src/com/codeeffector/App.java
+++ src/com/codeeffector/App.java
@@ -5,6 +5,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import javax.sound.sampled.*;
+
+import com.jsyn.JSyn;
+import com.jsyn.Synthesizer;
+import com.jsyn.data.FloatSample;
+import com.jsyn.unitgen.LineOut;
+import com.jsyn.unitgen.VariableRateMonoReader;
+import com.jsyn.util.SampleLoader;
+import com.jsyn.util.WaveRecorder;
+
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 public class App {
     private JButton delayButton;
     private JButton wahWahButton;
@@ -15,31 +26,114 @@ public class App {
     private JPanel panelMain;
     private JButton playEffectedSoundButton;
     private JButton loadSampleButton;
+    private JTextArea noSelectedSampleTextArea;
+    private File soundFile;
 
-    public App() {
+    private App() {
         button6.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    File soundFile = new File("sample.wav");
-                    AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
-                    Clip clip = AudioSystem.getClip();
-                    clip.open(audioIn);
-                    FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-                    volume.setValue(1.0f); // Reduce volume by 10 decibels.
-                    clip.start();
-                    JOptionPane.showMessageDialog(null, "Click OK to stop music");
-                    clip.drain();
-                } catch (UnsupportedAudioFileException ea) {
-                    ea.printStackTrace();
-                } catch (IOException ea) {
-                    ea.printStackTrace();
-                } catch (LineUnavailableException ea) {
+                    if(soundFile == null){
+                        getFile();
+                        return;
+                    }
+//                    AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+
+
+                    Synthesizer synth = JSyn.createSynthesizer();
+                    LineOut lineOut = new LineOut();
+                    synth.add(lineOut);
+                    synth.start();
+
+
+                    FloatSample samples = SampleLoader.loadFloatSample(soundFile);
+                    float[] dsamples = new float[samples.getNumFrames()*2];
+                    for (int i = 0; i < dsamples.length; i++)
+                        dsamples[i] = (float) samples.readDouble(i);
+                    double fr = samples.getFrameRate();
+                    samples = new FloatSample(dsamples);
+                    samples.setFrameRate(fr);
+                    VariableRateMonoReader player = new VariableRateMonoReader();
+                    synth.add(player);
+                    player.rate.set(samples.getFrameRate()*2);
+                    player.dataQueue.queue(samples);
+
+                    File outputWave = new File("output.wav");
+                    WaveRecorder recorder = new WaveRecorder(synth, outputWave);
+                    player.output.connect(0, recorder.getInput(), 0);
+                    recorder.start();
+
+                    player.output.connect(0, lineOut.input, 0);
+//                    player.output.connect(0, lineOut.input, 1);
+                    synth.startUnit(lineOut);
+                    do {
+                        synth.sleepFor(1.0);
+                    } while (player.dataQueue.hasMore());
+
+                    recorder.stop();
+                    recorder.close();
+                    synth.stop();
+                } catch (IOException | InterruptedException ea) {
                     ea.printStackTrace();
                 }
             }
         });
+
+        loadSampleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    getFile();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+
+
+            }
+        });
+        playEffectedSoundButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    float[] buff = getBuffer();
+                    FloatSample samples = new FloatSample(buff);
+
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+
+            }
+        });
     }
+    public void getFile() throws IOException {
+        final JFileChooser fc = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("MPEG3 songs", "mp3","wav");
+        fc.setFileFilter(filter);
+
+        int returnVal = fc.showOpenDialog(loadSampleButton);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+            noSelectedSampleTextArea.setText("Imported sample: " + file.getName());
+            soundFile = new File(file.getAbsolutePath());
+            System.out.println(file.getAbsolutePath());
+            //This is where a real application would open the file.
+        }
+
+    }
+    public float[] getBuffer() throws IOException {
+        FloatSample samples = SampleLoader.loadFloatSample(soundFile);
+
+        float[] dsamples = new float[samples.getNumFrames()];
+        for (int i = 0; i < dsamples.length; i++)
+            dsamples[i] = (float) samples.readDouble(i);
+
+        return dsamples;
+    }
+
+
+
+
 
     public static void main(String[] args)
             throws Exception
@@ -50,6 +144,8 @@ public class App {
         frame.pack();
         frame.setVisible(true);
     }
+
+
 }
 
 
--- src/com/codeeffector/Delay.java
+++ src/com/codeeffector/Delay.java
@@ -1,4 +1,62 @@
-package com.codeeffector;
-
-public class Delay {
-}
+//package com.codeeffector;
+//
+//public class Delay {
+//    public double dryWetMixPercent = 10.0;
+//    public double feedbackPercent = 30;
+//
+//    private short processSample(short inputSample) {
+//
+//        if (bypassed) {
+//            return inputSample;
+//        }
+//        // Read a delayed sample
+//        short delayedSample = delayBuffer[readIndex++];
+//
+//        double dryLevel = ((100.0 - dryWetMixPercent) * inputSample) / 100.0;
+//        double wetLevel = (dryWetMixPercent * delayedSample) / 100.0;
+//        short outputSample = (short) (dryLevel + wetLevel);
+//
+//        inputSample += (delayedSample * feedbackPercent) / 100.0;
+//
+//        // Write an input sample
+////        delayBuffer[writeIndex++] = inputSample;
+//
+//        // Update indices
+//  //      readIndex  %= DELAY_BUFFER_SIZE;
+//   //     writeIndex %= DELAY_BUFFER_SIZE;
+//
+//        return outputSample;
+//    }
+//
+//    /**
+//     * Process a buffer full of samples pulled from the sample provider
+//     *
+//     * @param buffer Buffer in which the samples are to be processed
+//     *
+//     * @return Count of number of bytes processed
+//     */
+//    public int getSamples(byte [] buffer) {
+//
+//        // Grab samples to manipulate from this modules sample provider
+//        provider.getSamples(buffer);
+//
+//        int index = 0;
+//        for (int i = 0; i < SamplePlayer.SAMPLES_PER_BUFFER; i++) {
+//            // Get a sample to process
+//            byte b2 = buffer[index];
+//            byte b1 = buffer[index+1];
+//
+//            // Convert bytes into short sample
+//            short s = (short)((((int) b2) << 8) + b1);
+//
+//            // Process the sample
+//            s = processSample(s);
+//
+//            // Store the processed sample
+//            buffer[index++] = (byte)(s >> 8);
+//            buffer[index++] = (byte)(s & 0xFF);
+//        }
+//        return SamplePlayer.BUFFER_SIZE;
+//    }
+//
+//}
